a5c5db682e81753194582a5d675fe6dbfe55a0d6
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2022-10-30 01:01:07 -0600

    Update routes.rs

--- src/users/routes.rs
+++ src/users/routes.rs
@@ -2,21 +2,40 @@ use crate::users::{User, Users};
 use crate::error_handler::CustomError;
 use actix_web::{delete, get, post, put, web, HttpResponse};
 use serde_json::json;
+use actix_web::HttpRequest;
+
+use chrono::Utc;
+use chrono::DateTime;
 
 #[get("/users")]
-async fn find_all() -> Result<HttpResponse, CustomError> {
+async fn find_all(req: HttpRequest) -> Result<HttpResponse, CustomError> {
+    let peer = req.peer_addr();
+    let requ = req.headers();
+    let readi: DateTime<Utc> = Utc::now();
+    println!("[{:?} INFO ] - squirrel tActix - /users POST function request - {:?} - {:?}", readi, peer, requ);
     let users = Users::find_all()?;
     Ok(HttpResponse::Ok().json(users))
 }
 
 #[get("/users/{id}")]
-async fn find(id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
+async fn find(req: HttpRequest, id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
+    let peer = req.peer_addr();
+    let requ = req.headers();
+    let readi: DateTime<Utc> = Utc::now();
+    println!("[{:?} INFO ] - squirrel tActix - /users/{id} GET function request - {:?} - {:?}", readi, peer, requ);
+
+
     let user = Users::find(id.into_inner())?;
     Ok(HttpResponse::Ok().json(user))
 }
 
 #[post("/users")]
-async fn create(user: web::Json<User>) -> Result<HttpResponse, CustomError> {
+async fn create(req: HttpRequest, user: web::Json<User>) -> Result<HttpResponse, CustomError> {
+    let peer = req.peer_addr();
+    let requ = req.headers();
+    let readi: DateTime<Utc> = Utc::now();
+    println!("[{:?} INFO ] - squirrel tActix - /users POST function request - {:?} - {:?}", readi, peer, requ);
+
     let user = Users::create(user.into_inner())?;
     Ok(HttpResponse::Ok().json(user))
 }
@@ -31,7 +50,12 @@ async fn update(
 }
 
 #[delete("/users/{id}")]
-async fn delete(id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
+async fn delete(req: HttpRequest, id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
+    let peer = req.peer_addr();
+    let requ = req.headers();
+    let readi: DateTime<Utc> = Utc::now();
+    println!("[{:?} INFO ] - squirrel tActix - /users/{id} DELETE function request - {:?} - {:?}", readi, peer, requ);
+
     let deleted_user = Users::delete(id.into_inner())?;
     Ok(HttpResponse::Ok().json(json!({ "deleted": deleted_user })))
 }
