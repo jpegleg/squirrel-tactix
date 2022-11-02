1e3943868e5a19b0d5bb47005bf49cb630032679
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2022-11-01 21:36:17 -0600

    adding health check feature

--- src/users/routes.rs
+++ src/users/routes.rs
@@ -3,16 +3,31 @@ use crate::error_handler::CustomError;
 use actix_web::{delete, get, post, put, web, HttpResponse};
 use serde_json::json;
 use actix_web::HttpRequest;
-
 use chrono::Utc;
 use chrono::DateTime;
+
+
+#[get("/health")]
+async fn healthchecks(req: HttpRequest) -> Result<HttpResponse, CustomError> {
+    let peer = req.peer_addr();
+    let requ = req.headers();
+    let readi: DateTime<Utc> = Utc::now();
+    println!("[{:?} INFO ] - squirrel-tactix - /health GET (health check) request - from {:?} - {:?}", readi, peer, &requ);
+    let mut users = Users::find(1)?;
+    let readb: DateTime<Utc> = Utc::now();
+    match users {
+      users => println!("[{:?} INFO ] - squirrel-tactix - /health GET (health check) response OK - {:?}", readb, requ),
+    };
+    let hresp = "RECV";
+    Ok(HttpResponse::Ok().json(hresp))
+}
 
 #[get("/users")]
 async fn find_all(req: HttpRequest) -> Result<HttpResponse, CustomError> {
     let peer = req.peer_addr();
     let requ = req.headers();
     let readi: DateTime<Utc> = Utc::now();
-    println!("[{:?} INFO ] - squirrel tActix - /users GET function request - from {:?} - {:?}", readi, peer, requ);
+    println!("[{:?} INFO ] - squirrel-tactix - /users GET function request - from {:?} - {:?}", readi, peer, requ);
     let users = Users::find_all()?;
     Ok(HttpResponse::Ok().json(users))
 }
@@ -22,7 +37,7 @@ async fn find(req: HttpRequest, id: web::Path<i32>) -> Result<HttpResponse, Cust
     let peer = req.peer_addr();
     let requ = req.headers();
     let readi: DateTime<Utc> = Utc::now();
-    println!("[{:?} INFO ] - squirrel tActix - /users/{id} GET function request - from {:?} - {:?}", readi, peer, requ);
+    println!("[{:?} INFO ] - squirrel-tactix - /users/{id} GET function request - from {:?} - {:?}", readi, peer, requ);
 
 
     let user = Users::find(id.into_inner())?;
@@ -34,7 +49,7 @@ async fn create(req: HttpRequest, user: web::Json<User>) -> Result<HttpResponse,
     let peer = req.peer_addr();
     let requ = req.headers();
     let readi: DateTime<Utc> = Utc::now();
-    println!("[{:?} INFO ] - squirrel tActix - /users POST function request - from {:?} - {:?}", readi, peer, requ);
+    println!("[{:?} INFO ] - squirrel-tactix - /users POST function request - from {:?} - {:?}", readi, peer, requ);
 
     let user = Users::create(user.into_inner())?;
     Ok(HttpResponse::Ok().json(user))
@@ -50,7 +65,7 @@ async fn update(
     let requ = req.headers();
     let user = Users::update(id.into_inner(), user.into_inner())?;
     let readi: DateTime<Utc> = Utc::now();
-    println!("[{:?} INFO ] - squirrel tActix - /users/x (update) PUT function request - from {:?} - {:?}", readi, peer, requ);
+    println!("[{:?} INFO ] - squirrel-tactix - /users/x (update) PUT function request - from {:?} - {:?}", readi, peer, requ);
 
 
     Ok(HttpResponse::Ok().json(user))
@@ -61,7 +76,7 @@ async fn delete(req: HttpRequest, id: web::Path<i32>) -> Result<HttpResponse, Cu
     let peer = req.peer_addr();
     let requ = req.headers();
     let readi: DateTime<Utc> = Utc::now();
-    println!("[{:?} INFO ] - squirrel tActix - /users/{id} DELETE function request - from {:?} - {:?}", readi, peer, requ);
+    println!("[{:?} INFO ] - squirrel-tactix - /users/{id} DELETE function request - from {:?} - {:?}", readi, peer, requ);
 
     let deleted_user = Users::delete(id.into_inner())?;
     Ok(HttpResponse::Ok().json(json!({ "deleted": deleted_user })))
@@ -73,4 +88,5 @@ pub fn init_routes(config: &mut web::ServiceConfig) {
     config.service(create);
     config.service(update);
     config.service(delete);
+    config.service(healthchecks);
 }
